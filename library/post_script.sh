e5ec708afad9b6e592da353a73789a0e48070a74
jvelilla <javier.hector@gmail.com>
2020-10-26 14:54:12 -0300

    Fixed ecf for project example. Updated linux scripts. Updated libtensorflow.ecf with Linux configuration.

--- library/post_script.sh
+++ library/post_script.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # Post processing script
 
-# copy c file is there any
-# cp ./manual_wrapper/c/src/*.c  ./generated_wrapper/c/src           --example
-# cp ./manual_wrapper/c/include/*.h  ./generated_wrapper/c/include		
+echo Copy MakeFile
+cp Makefile.SH  ./generated_wrapper/c/src
 
-
-#copy Makefile
-#cp Makefile.SH  ./generated_wrapper/c/src                          --example
+cd generated_wrapper/c/src/
+finish_freezing -library
